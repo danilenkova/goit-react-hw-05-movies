c73c2910b5625c8b8ae3fba588bd6aa1e3c879a3
Yuliia Danilenkova <82413695+danilenkova@users.noreply.github.com>
2022-04-12 14:50:22 +0300

    Add text for NotFoundView

--- src/views/NotFoundView.js
+++ src/views/NotFoundView.js
@@ -1,12 +1,23 @@
-import { Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { ImArrowLeft2 } from "react-icons/im";
+import s from "../styles/styles.module.css";
 
 export default function NotFoundView() {
   const location = useLocation;
   const goToMain = location?.state?.from ?? "/";
   return (
     <>
-      <Link to={goToMain}>Go Back</Link>
-      <h1>404 Not Found</h1>
+      <NavLink to={goToMain} className={s.goBackLink}>
+        <ImArrowLeft2 />
+        Go Back
+      </NavLink>
+      <div className={s.notFound}>
+        <p>
+          404
+          <br />
+          Page Not Found
+        </p>
+      </div>
     </>
   );
 }
